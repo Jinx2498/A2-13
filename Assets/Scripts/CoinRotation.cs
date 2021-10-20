cbc66c9608cfafad68b1eddb7d155747fe309615
Quentin <q.lambkin@gmail.com>
2021-10-20 17:24:58 -0400

    Coint script and object

---
+++ Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(30 * Time.deltaTime, 15 * Time.deltaTime, 45 * Time.deltaTime, Space.World);
+    }
+}
