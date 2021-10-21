7933d1b5ff8ada2602740fa37da3d6a3a5766873
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-10-20 22:32:58 -0400

    level 2

--- Assets/Scripts/CoinRotation.cs
+++ Assets/Scripts/CoinRotation.cs
@@ -6,13 +6,11 @@ public class CoinRotation : MonoBehaviour
 {
     void Update()
     {
-        // Rotation Formula
         transform.Rotate(30 * Time.deltaTime, 15 * Time.deltaTime, 45 * Time.deltaTime, Space.World);
     }
 
     void OnCollisionEnter(Collision other)
     {
-        // Object Destroyed on Contact
         Destroy(gameObject);
     }
 }
