2a6b290513b4c6a3c250efbd4ac43ffbbc704b0f
Quentin <50932070+QLambkin@users.noreply.github.com>
2021-10-20 21:54:40 -0400

    jump script update

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,9 +1,40 @@
 using UnityEngine;
+using System.Collections;
+
 public class MovePlayer : MonoBehaviour
 {
     public Rigidbody rb;
     public float force = 50f;
     public int jumpCount = 2;
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (jumpCount == 2)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 5.2f, rb.velocity.z);
+                --jumpCount;
+            }
+            else
+            {
+                if (jumpCount == 1)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, 5.2f, rb.velocity.z);
+                    --jumpCount;
+                    StartCoroutine(Wait());
+                }
+            }
+        }
+
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(1f);
+            jumpCount = 2;
+        }
+    }
 
     void FixedUpdate()
     {
@@ -25,20 +56,6 @@ public class MovePlayer : MonoBehaviour
         if (Input.GetKey("d"))
         {
             rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (Input.GetKeyDown("space"))
-        {
-            if(jumpCount > 0)
-            {
-                rb.velocity = new Vector3(rb.velocity.x, 5.2f, rb.velocity.z);
-                --jumpCount;
-            }
-        }
-
-        if(rb.velocity.y == 0)
-        {
-            jumpCount = 2;
         }
     }
 }
