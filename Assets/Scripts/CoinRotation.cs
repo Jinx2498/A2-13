3aef6549de5a7a3bf009e4891d10ea28c42df034
Quentin <50932070+QLambkin@users.noreply.github.com>
2021-10-20 22:09:43 -0400

    Update CoinRotation.cs

--- Assets/Scripts/CoinRotation.cs
+++ Assets/Scripts/CoinRotation.cs
@@ -6,11 +6,13 @@ public class CoinRotation : MonoBehaviour
 {
     void Update()
     {
+        // Rotation Formula
         transform.Rotate(30 * Time.deltaTime, 15 * Time.deltaTime, 45 * Time.deltaTime, Space.World);
     }
 
     void OnCollisionEnter(Collision other)
     {
+        // Object Destroyed on Contact
         Destroy(gameObject);
     }
 }
